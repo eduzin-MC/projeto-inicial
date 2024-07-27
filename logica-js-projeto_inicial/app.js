69a8e8e47871b577794e986b6cf4b2e00fe9c6c1
eduzin-mc <ethanbaronv@example.com>
2024-07-27 18:40:53 -0300

    projeto inicial

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+console.log('boas vindas');
+let nome = 'edu';
+alert (`ola, ${nome}!`); 
+console.log(`ola, ${nome}!`);
+let linguegemPreferida = prompt('qual a linguagem de progamação que você mais gosta?');
+let valor1 = 42;
+let valor2 = 8;
+let resultado = valor1 + valor2;
+
+console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`);
